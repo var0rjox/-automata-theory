b527f60471f8de6c764349cb9c70c867f2af93f9
bobcodevar <vargasorellanajoseroberto@gmail.com>
2024-01-22 19:08:54 -0400

    problemas con el analisis del compilador , se realizara la implemtacion por partes

--- ParserG.hs
+++ ParserG.hs
@@ -1,23 +1,36 @@
 module ParserG where
+
 import UU_Parsing
 import Scanner
 
-
 data Etiquetas = Et Etiquetas
-               | Et1 Etiquetas Etiquetas
-               | Et2 Etiquetas
-               | Et3 Etiquetas
-               | Et4 String
-               | Et5 String
-               | Et6 String
-               | VacioS
-          deriving Show
+                | Et1 Etiquetas Etiquetas
+                | Et2 Etiquetas
+                | Et3 Etiquetas
+                | Et4 String
+                | Et5 String
+                | Et6 String
+                | VacioS
+              deriving Show
+pDoctype = Et <$ pOpClave "<!" <* pPalClave "doctype html" <* pSimbolo ">" <*> pHtml
 
-pEtiquetas = Et <$ pOpClave "<!" <* pPalClave"doctype html" <* pSimbolo ">" <*> pEtiquetas
-      <|>  (\a b -> Et1 a b) <$ pSimbolo "<" <* pPalClave "html" <* pSimbolo ">" <*> pEtiquetas <*> pEtiquetas <* pOpClave "</" <* pPalClave "html" <* pSimbolo ">"
-      <|>  (\a -> Et2 a) <$ pSimbolo "<" <* pPalClave "head" <* pSimbolo ">" <*> pEtiquetas <* pOpClave "</" <* pPalClave "head" <* pSimbolo ">"
-      <|>  (\a -> Et3 a) <$ pSimbolo "<" <* pPalClave "body" <* pSimbolo ">" <*> pEtiquetas <* pOpClave "</" <* pPalClave "body" <* pSimbolo ">" -- checar la parte recursiva de llamada a estos elementos
-      <|>  (\a -> Et4 a) <$ pSimbolo "<" <* pPalClave "meta" <* pPalClave "charset" <* pSimbolo "=" <*> pCadena <* pOpClave "</" <* pPalClave "head" <* pSimbolo ">"
-      <|>  (\a -> Et5 a) <$ pSimbolo "<" <* pPalClave "title" <* pSimbolo ">" <*> pIdent <* pOpClave "</" <* pPalClave "title" <* pSimbolo ">"
-      <|> (\a -> Et6 a) <$ pSimbolo "<" <* pPalClave "p" <* pSimbolo ">" <*> pCadena <* pOpClave "</" <* pPalClave "p" <* pSimbolo ">"
-      <|> pSucceed VacioS
+pHtml = (\a b -> Et1 a b) <$ pSimbolo "<" <* pPalClave "html" <* pSimbolo ">" <*> pHead <*> pBody <* pOpClave "</" <* pPalClave "html" <* pSimbolo ">"
+
+pHead = (\a -> Et2 a) <$ pSimbolo "<" <* pPalClave "head" <* pSimbolo ">" <*> (pMeta <|> pTitle) <* pOpClave "</" <* pPalClave "head" <* pSimbolo ">"
+
+pBody = (\a -> Et3 a) <$ pSimbolo "<" <* pPalClave "body" <* pSimbolo ">" <*> pParrafo <* pOpClave "</" <* pPalClave "body" <* pSimbolo ">"
+
+pMeta = (\a -> Et4 a) <$ pSimbolo "<" <* pPalClave "meta" <* pPalClave "charset" <* pSimbolo "=" <*> pCadena <* pOpClave "</" <* pPalClave "head" <* pSimbolo ">"
+
+pTitle = (\a -> Et5 a) <$ pSimbolo "<" <* pPalClave "title" <* pSimbolo ">" <*> pIdent <* pOpClave "</" <* pPalClave "title" <* pSimbolo ">"
+
+pParrafo = (\a -> Et6 a) <$ pSimbolo "<" <* pPalClave "p" <* pSimbolo ">" <*> pCadena <* pOpClave "</" <* pPalClave "p" <* pSimbolo ">"
+
+pEtiquetas = pDoctype
+          <|> pHtml
+          <|> pHead
+          <|> pBody
+          <|> pMeta
+          <|> pTitle
+          <|> pParrafo
+          <|> pSucceed VacioS
